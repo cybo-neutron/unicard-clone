caf8abf2efe0db40680394703f31f68f4900046a
Cybo-Neutron <cyboneutron@gmail.com>
2022-12-22 17:17:30 +0530

    checkpoint

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Section4() {
   return (
-    <div className="bg-zinc-300 flex flex-col px-10 py-5 gap-y-5">
-      <div className="text-2xl font-semibold">
+    <div className="bg-zinc-800 flex flex-col px-10 py-5 gap-y-5 text-white">
+      <div className="text-2xl font-semibold ">
         At Uni, we’re committed to delivering an unmatched credit experience for
         millions of Indians.
       </div>
 
-      <div className="text-xl">
+      <div className="text-xl text-zinc-400">
         On this mission, we’ve partnered with some of the best in the business.
       </div>
 
